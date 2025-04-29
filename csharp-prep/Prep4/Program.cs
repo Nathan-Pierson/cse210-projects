8f6a2bc459d056a0db2aada47c87e85fea7e08eb
Nathan Pierson <johndoe@email.com>
2025-04-28 20:49:42 -0600

    This is the completed Prep 4 work without the stretch challenge

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int stop = 1;
+        while (stop != 0){
+            Console.Write("Please enter a number: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            if (n == 0){
+                int sum = 0;
+                foreach (int number in numbers){
+                    sum += number;
+                }
+                Console.WriteLine($"The Sum of your numbers is: {sum}");
+                double average = (double)sum / (double)numbers.Count();
+                Console.WriteLine($"The Average is: {average:F2}");
+                int max = numbers.Max();
+                Console.WriteLine($"The maximum number is: {max}");
+                stop = 0;
+
+                }
+            else{
+            numbers.Add(n);
+            }
+        }
     }
 }
