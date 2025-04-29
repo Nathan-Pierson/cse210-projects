4fba3987fe153b298ac889018d37edab14c586a2
Nathan Pierson <johndoe@email.com>
2025-04-28 21:04:05 -0600

    Completed prep 5 without stretch challenge

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+        string name = UserName();
+        int number = UserNumber();
+        int n = square(number);
+        Console.WriteLine($"{name}, the square root of your number is {n}");
     }
+
+    static void Welcome(){
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static string UserName(){
+        Console.Write("Enter Your Name: ");
+        string Name = Console.ReadLine();
+
+        return Name;
+    }
+
+    static int UserNumber(){
+        Console.Write("Enter a number: ");
+        string s = Console.ReadLine();
+        int number = int.Parse(s);
+
+        return number;
+    }
+
+    static int square(int number){
+        int numberSquared = number * number;
+
+        return numberSquared; 
+    }
+
 }
