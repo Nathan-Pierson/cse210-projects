98b4992fa6f556c5020c68eb5e2f8d476b38c30a
Nathan Pierson <johndoe@email.com>
2025-05-19 10:17:34 -0600

    Prep work all completed.

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,40 @@
+/// <summary>
+/// This class is used for making a fraction with a numerator and denominator.
+/// This is more accurate than storing numbers in a double.
+/// </summary>
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int whole)
+    {
+        _numerator = whole;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string representation = $"{_numerator}/{_denominator}";
+        return representation;
+    }
+
+    public double GetDecimalValue()
+    {
+        double value = (double)_numerator / (double)_denominator;
+        return value;
+    }
+}
